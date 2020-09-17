749ad3afbdc0e14669a25e7899b99ea9fd2af8e3
Francisco Politi <ppoliti8@gmail.com>
2020-09-17 15:17:22 +0200

    Updates

---
+++ precache-manifest.51871eb83f455a13d8d6080689162cc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0113b04db9d591f89fb3d66b4be593d0",
+    "url": "/shopping-list/index.html"
+  },
+  {
+    "revision": "6f7ec022f00788f6705f",
+    "url": "/shopping-list/static/css/main.4f248665.chunk.css"
+  },
+  {
+    "revision": "589c02329bf599d86591",
+    "url": "/shopping-list/static/js/2.958a131e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping-list/static/js/2.958a131e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f7ec022f00788f6705f",
+    "url": "/shopping-list/static/js/main.56e60c8f.chunk.js"
+  },
+  {
+    "revision": "b5089a779ecd5276ccfe",
+    "url": "/shopping-list/static/js/runtime-main.f3110c9b.js"
+  }
+]);
